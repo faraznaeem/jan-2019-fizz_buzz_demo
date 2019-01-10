f209550a97aac5aaf8b923e298f1fd8912ba4b7e
faraznaeem <faraznaeem@gmail.com>
2019-01-10 16:55:22 +0100

    Refactors code with new method

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,12 +1,16 @@
 
 def fizz_buzz(number)
-    if number % 15 == 0
+    if has_zero_remainder(number, 15)
         'Fizz Buzz'
-    elsif number % 5 == 0
+    elsif has_zero_remainder(number, 5)
         'Buzz'
-    elsif number % 3 == 0
+    elsif has_zero_remainder(number, 3)
         'Fizz'
     else
         number
     end
+end
+
+def has_zero_remainder(number, divider)
+    number % divider == 0
 end
