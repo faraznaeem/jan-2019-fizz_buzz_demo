f5672cb308887343285f29a23e01719ed49f34ba
faraznaeem <faraznaeem@gmail.com>
2019-01-10 16:44:53 +0100

    Adds spec and implementation code for numbers divisable by 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,6 +2,8 @@
 def fizz_buzz(number)
     if number % 3 == 0
         'Fizz'
+    elsif number % 5 == 0
+        'Buzz'
     else
         number
     end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -9,4 +9,9 @@ describe 'fizz_buzz' do
     it 'returns Fizz if number is divisible by 3' do
         expect(fizz_buzz(3)).to eq 'Fizz'
     end
+
+    it 'returns Buzz if number is divisible by 5' do
+        expect(fizz_buzz(5)).to eq 'Buzz'
+    end
+
 end
